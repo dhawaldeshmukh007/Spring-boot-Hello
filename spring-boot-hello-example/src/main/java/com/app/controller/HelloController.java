6799b0dc9f962782bfceb6c07fca9e7176ace840
Dhawal <Dhawal@DESKTOP-G05BL35>
2022-05-23 11:10:19 +0530

    Spring Hello Example

---
+++ spring-boot-hello-example/src/main/java/com/app/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping(value = "/")
+	public String viewwelcome(Model model) {
+
+		model.addAttribute("msg",
+				"Dhawal is a Hindi Boy names that is adored by everyone. Dhawal name is driven from Hindi Language. It is a popular name in Hindu community as this names is meaningful yet attractive. Dhawal name meaning is White and this is a perfect name to give as a life time identity to your baby.");
+		return "welcome";
+
+	}
+
+}
---
+++ spring-boot-hello-example/src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>DAD</title>
+</head>
+<body>
+<h1>Hello,Your are in to the DAD's World!!!!</h1>
+<h4>@__All Rights Reserved_</h4>
+<h3>${msg}</h3>
+</body>
+</html>
